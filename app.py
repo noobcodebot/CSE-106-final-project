d0b1328c51d9402b41f7df9423ae4e79bd8bbbc5
Ta Jour Harris <tjharris705@yahoo.com>
2021-11-22 18:16:23 -0800

    Fixed logic for registering a user with a username and password and added code logic for logging in with hashed and salted passwords

--- app.py
+++ app.py
@@ -2,35 +2,40 @@ import os.path
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-import bcrypt
-from bcrypt import gensalt
-
+import hashlib, uuid
+from flask_login import LoginManager, current_user, login_user, login_required, logout_user
 import models
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.sqlite')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 db = SQLAlchemy(app)
 engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"], echo=True)
-
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# login_manager.login_view = 'login'
-# app.secret_key = 'keep secret'  # placeholder
-salt = '$aggronblaziken$'
-
 
 db.create_all()
 db.session.commit()
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+app.secret_key = 'keep secret'  # placeholder
+salt = uuid.uuid4().hex.encode('utf-8')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return models.Users.query.get(user_id)
+
 
 def encrypt_password(password):
-    return bcrypt.hashpw(password, gensalt(12))
+    encoded_password = password.encode('utf-8')
+    return hashlib.sha256(encoded_password + salt).hexdigest()
 
 
-def check_password(plaintext, hashed):
-    return bcrypt.checkpw(plaintext, hashed)
+def check_password(plain, password):
+    encoded_plain = plain.encode('utf-8')
+    valid = (hashlib.sha256(encoded_plain + salt).hexdigest() == password)
+    return valid
 
 
 def add_user(username, password):
@@ -65,6 +70,28 @@ def register():
         else:
             return render_template('register.html', error='Please enter valid credentials!')
     return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        student = models.Students.query.filter_by(user_id=current_user.id).first()
+        return render_template('user_page.html', name=student.first_name)
+    if request.method == 'POST':
+        user = models.Users.query.filter_by(username=request.form['username']).first()
+        if user is None or not check_password(request.form['password'], user.password):
+            return render_template('login.html', error='Please enter valid Username/Password')
+        login_user(user)
+        student = models.Students.query.filter_by(user_id=user.id).first()
+        return render_template('user_page.html', name=student.first_name)
+    return render_template('login.html', error='')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
