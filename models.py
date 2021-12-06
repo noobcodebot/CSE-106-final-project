65c038589f3a423422cbdf6185cd318ce323cc30
Ta Jour Harris <tjharris705@yahoo.com>
2021-12-05 22:46:37 -0800

    Updated db and models so class numbers and buildings are an attribute of classes model

--- models.py
+++ models.py
@@ -1,5 +1,9 @@
-from app import db
 from flask_login import UserMixin
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
 
 
 class Users(UserMixin, db.Model):
@@ -13,6 +17,8 @@ class Classes(db.Model):
     class_name = db.Column(db.String(100), nullable=False)
     timeslot = db.Column(db.String(100), nullable=False)
     students = db.relationship('Students', secondary='enrollment')
+    building = db.Column(db.String(10), nullable=False)
+    room_no = db.Column(db.String(5), nullable=False)
 
 
 class Students(db.Model):
@@ -28,5 +34,3 @@ class Enrollment(db.Model):
     id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
     class_id = db.Column('class_id', db.Integer, db.ForeignKey('classes.id'))
     student_id = db.Column('student_id', db.Integer, db.ForeignKey('students.id'))
-    building = db.Column(db.String(10), nullable=False)
-    room_no = db.Column(db.String(5), nullable=False)
