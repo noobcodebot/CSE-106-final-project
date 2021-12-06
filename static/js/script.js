484633205b6501fc334e96c4b010e7e99acf053d
Ta Jour Harris <tjharris705@yahoo.com>
2021-12-05 21:15:03 -0800

    Frontend function added for loading class map by id

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,8 @@ function jumpToLink(endpoint) {
     const url = "http://127.0.0.1:5000/"+endpoint;
     window.open(url, "_self");
 }
+
+function loadClassMap(id) {
+    const url = "http://127.0.0.1:5000/class/"+id+"/map";
+        window.open(url, "_self");
+}
