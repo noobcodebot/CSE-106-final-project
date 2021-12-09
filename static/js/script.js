bcb77450f750b5fcab35caf1360209e6782989d0
Ta Jour Harris <tjharris705@yahoo.com>
2021-12-09 15:17:39 -0800

    hardcoded function for loading map onclick. Need to make it dynamic.

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
+ window.addEventListener("load", function () {
+     var svgObject = document.getElementById('svg-campus').contentDocument;
+     var svg = svgObject.getElementById('cob1_path');
+     svg.onclick = function(){
+         let endpoint = "/class/1/map"
+        jumpToLink(endpoint);
+     };
+ })
 function jumpToLink(endpoint) {
     const url = "http://127.0.0.1:5000/"+endpoint;
     window.open(url, "_self");
@@ -5,5 +13,7 @@ function jumpToLink(endpoint) {
 
 function loadClassMap(id) {
     const url = "http://127.0.0.1:5000/class/"+id+"/map";
-        window.open(url, "_self");
+    window.open(url, "_self");
 }
+
+
